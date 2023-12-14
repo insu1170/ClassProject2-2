71cd958d0d3b6020c81c6e8eded09652682f9ef3
jinbark1229 <jinbark1229@naver.com>
2023-12-14 14:12:03 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MainImg from "./component/MainImg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
+import SLPS from "./page/SLPS";
+import Seafood from "./page/Seafood";
+import New from "./page/New";
+import Reference from "./page/Reference";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainImg/>} />
+        <Route path="/Alps" element={<SLPS/>}></Route>
+        <Route path="/Seafood" element={<Seafood/>}></Route>
+        <Route path="/New" element={<New/>}></Route>
+        <Route path="/Reference" element={<Reference/>} ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/component/SImg.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Style/SImg.css";
+import "../Style/Subpage.css"
+
+function SImg({ src,text }) {
+  return (
+    <div className="">
+      <div className="">
+        <img src={process.env.PUBLIC_URL + src} alt="없어요" className="ImgS subTopImg"/>
+        <p className="imgText">{text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SImg;
